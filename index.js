fddbf2749b65a68d5950da8e49fe4a204312e593
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-11 20:46:17 +0600

    fetch all done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,32 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ux1hh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    console.log("db connected");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send({ success: true, data: services });
+    });
+  } finally {
+    // console.log(error);
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hellow doctors");
