86d2f5a0bfdc53b9f88151056d27bde3f3061ee5
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-17 00:11:03 +0600

    heroku err

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //----- middleware -----
-const corsConfig = {
-  origin: true,
-  credentials: true,
-};
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 //-----------------------
 
