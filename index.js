0550cf1498f346523181121716ad645970a65572
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-16 22:57:51 +0600

    final port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const app = express();
 
 //----- middleware -----
