b155e6093a7513197f51b3c7ab1c08892482103a
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-16 22:55:57 +0600

    final

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 //----- middleware -----
 app.use(cors());
 app.use(express.json());
-//----------------------
+//-----------------------
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ux1hh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
